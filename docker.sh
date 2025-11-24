a4e8d07ac044ef8f53a540fb7885e1ca17fefe51
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2025-11-24 17:58:21 +0530

    infra through terraform

---
+++ docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#resize the disk
+lsblk
+# sudo growpart /dev/nvme0n1 4
+sudo growpart /dev/xvda 4
+sudo lvextend -l +50%FREE /dev/RootVG/rootVol
+sudo lvextend -l +50%FREE /dev/RootVG/varVol
+sudo xfs_growfs /
+sudo xfs_growfs /var
+
+#docker installation
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo systemctl enable --now docker
+
+sudo systemctl start docker
+sudo usermod -aG docker ec2-user
+sudo chmod 777 /var/run/docker.sock
+
+#setup kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+
+#eksctl setup
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
---
+++ main.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "this" {
+  ami           = var.ami_id
+  instance_type = "t2.medium"
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+  }
+  user_data = file("docker.sh")
+  tags = {
+    Name = "k8s"
+  }
+}
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow-ssh"
+  description = "this sg for k8s"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "sg-k8s"
+  }
+}
