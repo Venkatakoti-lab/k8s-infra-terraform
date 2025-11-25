ce41f4f8d183cc64deac48b626b323e5c8ba9e81
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2025-11-25 13:11:32 +0530

    updated output file

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "public_ip" {
-  value = aws_instance.this.public_ip
+output "ips" {
+  value= {
+    public_ip = aws_instance.this.public_ip
+    private_ip = aws_instance.this.private_ip
+  }
+  
 }
